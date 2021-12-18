4146261c8966e4587ef3f736ddb4e29df01a1e17
Sagar Routh <sagarrth@sagarrth-musings.local>
2021-12-18 15:45:06 +0530

    Show the card as slider, reduce the timer to one liner, CSS improvements, host the images in webp format

--- js/script.js
+++ js/script.js
@@ -1,46 +1,54 @@
 (function ($) {
-    "use strict";
-      $('.sakura-falling').sakura();
+  "use strict";
+    $('.sakura-falling').sakura();
 })(jQuery);
 
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 25, 2022 00:00:00").getTime();
+$(document).ready(function(){
+  $('#card-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    mobileFirst: true,
+    arrows: false,
+    centerMode: true
+  });
 
-// Update the count down every 1 second
-var x = setInterval(function() {
 
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = `
-      <div class='container'>
-        <div class='days block'>
-          ${days}<br>Days
-        </div>
-        <div class='hours block'>
-          ${hours}<br>Hours
-        </div>
-        <div class='minutes block'>
-          ${minutes}<br>Minutes
-        </div>
-        <div class='seconds block'>
-          ${seconds}<br>Seconds
-        </div>
-      </div>`;
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
-    }
-}, 1000);
+  // Set the date we're counting down to
+  const countDownDate = new Date("Jan 25, 2022 00:00:00").getTime();
+
+  const timeUnits = ['days', 'hours', 'minutes', 'seconds'];
+
+  const timeElementNodes = timeUnits.reduce((hash, unit) => {
+    hash[unit] = document.getElementById(unit);
+    return hash;
+  }, {});
+
+  // Update the count down every 1 second
+  const timerId = setInterval(function() {
+      // Get todays date and time
+      const now = new Date().getTime();
+      
+      // Find the distance between now and the count down date
+      const distance = countDownDate - now;
+      
+      // Time calculations for days, hours, minutes and seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      timeElementNodes.days.textContent = days < 10 ? `0${days}` : days;
+      timeElementNodes.hours.textContent = hours < 10 ? `0${hours}` :  hours;
+      timeElementNodes.minutes.textContent = minutes < 10 ? `0${minutes}` : minutes;
+      timeElementNodes.seconds.textContent = seconds < 10 ? `0${seconds}` : seconds;
+      
+      // If the count down is over, write some text 
+      if (distance < 0) {
+          clearInterval(timerId);
+          document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
+      }
+  }, 1000);
+});
