2bd34e08ace02de54912094db355888ae64f643b
Sagar Routh <sagarrth@sagarrth-musings.local>
2021-12-19 02:57:47 +0530

    Add scroll to top button, Refactor script.js by enclosing distinct features in functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,99 @@
 $(document).ready(function(){
-	$('#card-slider').slick({
+  function attachSaveTheDateCounter() {
+    // Set the date we're counting down to
+    const countDownDate = new Date("Jan 25, 2022 00:00:00").getTime();
+
+    const timeUnits = ['days', 'hours', 'minutes', 'seconds'];
+
+    const timeElementNodes = timeUnits.reduce((hash, unit) => {
+      hash[unit] = document.getElementById(unit);
+      return hash;
+    }, {});
+
+    // Update the count down every 1 second
+    const timerId = setInterval(function() {
+      // Get todays date and time
+      const now = new Date().getTime();
+      
+      // Find the distance between now and the count down date
+      const distance = countDownDate - now;
+      
+      // Time calculations for days, hours, minutes and seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      timeElementNodes.days.textContent = days < 10 ? `0${days}` : days;
+      timeElementNodes.hours.textContent = hours < 10 ? `0${hours}` :  hours;
+      timeElementNodes.minutes.textContent = minutes < 10 ? `0${minutes}` : minutes;
+      timeElementNodes.seconds.textContent = seconds < 10 ? `0${seconds}` : seconds;
+        
+      // If the count down is over, write some text 
+      if (distance < 0) {
+        clearInterval(timerId);
+        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
+      }
+    }, 1000);
+  }
+
+  function attachConfettiLogic() {
+    const randomInRange = (min, max) => Math.random() * (max - min) + min;
+
+    const button = document.querySelector(".happiness");
+    button.addEventListener("click", () => {
+      const duration = 5 * 1000;
+      const animationEnd = Date.now() + duration;
+      const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+      const interval = setInterval(function() {
+        const timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+          return clearInterval(interval);
+        }
+
+        const particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+        confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+      }, 250);
+    });
+  }
+  
+  function attachScrollToTopLogic() {
+    const rootElement = document.documentElement
+    const target = document.querySelector(".happiness");
+    const scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+
+    scrollToTopBtn.addEventListener('click', () => {
+      rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+
+    const callback = (entries, observer) => {
+      // The callback will return an array of entries, even if you are only observing a single item
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // Show button
+          scrollToTopBtn.classList.add('showBtn')
+        } else {
+          // Hide button
+          scrollToTopBtn.classList.remove('showBtn')
+        }
+      });
+    }
+
+    const observer = new IntersectionObserver(callback);
+    observer.observe(target);
+  }
+
+  // counter logic
+  attachSaveTheDateCounter();
+  // slider logic
+  $('#card-slider').slick({
 		dots: true,
 		infinite: true,
 		speed: 500,
@@ -10,66 +104,10 @@ $(document).ready(function(){
 		autoplaySpeed: 2500,
 		arrows: screen.width >= 768
 	});
-
-
-	// Set the date we're counting down to
-	const countDownDate = new Date("Jan 25, 2022 00:00:00").getTime();
-
-	const timeUnits = ['days', 'hours', 'minutes', 'seconds'];
-
-	const timeElementNodes = timeUnits.reduce((hash, unit) => {
-		hash[unit] = document.getElementById(unit);
-		return hash;
-	}, {});
-
-	// Update the count down every 1 second
-	const timerId = setInterval(function() {
-		// Get todays date and time
-		const now = new Date().getTime();
-		
-		// Find the distance between now and the count down date
-		const distance = countDownDate - now;
-		
-		// Time calculations for days, hours, minutes and seconds
-		const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-		timeElementNodes.days.textContent = days < 10 ? `0${days}` : days;
-		timeElementNodes.hours.textContent = hours < 10 ? `0${hours}` :  hours;
-		timeElementNodes.minutes.textContent = minutes < 10 ? `0${minutes}` : minutes;
-		timeElementNodes.seconds.textContent = seconds < 10 ? `0${seconds}` : seconds;
-			
-		// If the count down is over, write some text 
-		if (distance < 0) {
-				clearInterval(timerId);
-				document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
-		}
-	}, 1000);
-
-  const randomInRange = (min, max) => Math.random() * (max - min) + min;
-
-  const button = document.querySelector(".happiness");
-	button.addEventListener("click", () => {
-    const duration = 5 * 1000;
-    const animationEnd = Date.now() + duration;
-    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-
-    const interval = setInterval(function() {
-      const timeLeft = animationEnd - Date.now();
-
-      if (timeLeft <= 0) {
-        return clearInterval(interval);
-      }
-
-      const particleCount = 50 * (timeLeft / duration);
-      // since particles fall down, start a bit higher than random
-      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
-      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
-    }, 250);
-  });
-
   // leaves falling animation
 	$('.sakura-falling').sakura();
+  // confetti logic
+  attachConfettiLogic();
+  // scroll to top logic
+  attachScrollToTopLogic();
 });
